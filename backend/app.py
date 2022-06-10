b2981d74883ad72a659d632ef0bc6d77f4574711
proguy914629 <proguy914629@openrobot.xyz>
2022-06-10 12:28:39 +0700

    Make DALL-E Flask app return more details
    Remove CORS (useless for now) and add time and passed data in "/dalle" endpoint & better handling

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,12 @@
 import base64
 import sys
+import time
 from io import BytesIO
 
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
 from consts import ModelSize
 
 app = Flask(__name__)
-CORS(app)
 print("--> Starting DALL-E Server. This might take up to two minutes.")
 
 from dalle_model import DalleModel
@@ -15,12 +14,25 @@ dalle_model = None
 
 
 @app.route("/dalle", methods=["POST"])
-@cross_origin()
 def generate_images_api():
     json_data = request.get_json(force=True)
-    text_prompt = json_data["text"]
-    num_images = json_data["num_images"]
+    
+    try:
+        text_prompt = str(json_data["text"])
+        num_images = int(json_data["num_images"])
+    except KeyError:
+        return jsonify({'error': {'code': 400}, 'message': 'Missing "text" and/or "num_images" keys in JSON data'})
+    
+    start = time.perf_counter()
     generated_imgs = dalle_model.generate_images(text_prompt, num_images)
+    end = time.perf_counter()
+    
+    unknown_data = json_data.copy()
+    
+    for key in ('text', 'num_images'):
+        unknown_data.pop(key)
+    
+    js = {'time': {'start': start, 'end': end, 'elapsed': end-start, 'average': (end-start)/num_images}, 'passed': {'text': text_prompt, 'num_images': num_images, 'unknown': unknown_data}}
 
     generated_images = []
     for img in generated_imgs:
@@ -28,13 +40,14 @@ def generate_images_api():
         img.save(buffered, format="JPEG")
         img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
         generated_images.append(img_str)
+        
+    js['images'] = generated_images
 
     print(f"Created {num_images} images from text prompt [{text_prompt}]")
     return jsonify(generated_images)
 
 
 @app.route("/", methods=["GET"])
-@cross_origin()
 def health_check():
     return jsonify(success=True)
 
